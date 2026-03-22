4a345190872fe89b2782d0d55e2e81bb17219f22
Vladimir Romanov <rirusha@altlinux.org>
2026-03-22 12:30:38 +0300

    fix: Fix cimpiling with lib

--- tools/auto-data.py
+++ tools/auto-data.py
@@ -1,10 +1,37 @@
+'''
+Copyright (C) 2026 Vladimir Romanov <rirusha@altlinux.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see
+<https://www.gnu.org/licenses/gpl-3.0-standalone.html>.
+
+SPDX-License-Identifier: GPL-3.0-or-later
+'''
+
 from pathlib import Path
 import sys
 import os
 import subprocess
 
 def compile_blueprint() -> None:
-    subprocess.run(['blueprint-compiler', 'batch-compile', OUTPUT, CURRENT_SOURCE_DIR, *get_blp_files(False)])
+    subprocess.run([
+        'blueprint-compiler',
+        'batch-compile',
+        '--gir-path', os.path.join(BUILD_ROOT, 'lib', 'src'),
+        '--typelib-path', os.path.join(BUILD_ROOT, 'lib', 'src'),
+        OUTPUT, CURRENT_SOURCE_DIR,
+        *get_blp_files(False)
+    ])
 
 def get_blp_files(fix_path:bool=True) -> list[str]:
     return [str(f).removeprefix('../data/' if fix_path else '') for f in Path(UI_DIR).rglob('*.blp') if f.is_file()]
